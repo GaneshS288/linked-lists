ffacd5b12491d41f1eaf2dcf36c826f6782ec57b
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-11-09 16:54:59 +0530

    install eslint and prettier

--- linked-list.js
+++ linked-list.js
@@ -1,7 +1,7 @@
 export class LinkedList {
-    constructor(head = null, tail = null, size = 0) {
-        this.head = head;
-        this.tail = tail;
-        this.size = size;
-    }
+  constructor(head = null, tail = null, size = 0) {
+    this.head = head;
+    this.tail = tail;
+    this.size = size;
+  }
 }
